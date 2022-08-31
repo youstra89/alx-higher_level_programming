ce3c908b42d8563f93a27594ce31de500d04fa52
youstra89 <contact.youstra@gmail.com>
2022-08-31 11:49:46 +0000

    Review 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def islower(c):
     response = None
-    if c.islower():
-        response = True
-    else:
-        response = False
-    
+    if c != '':
+        if c.islower():
+            response = True
+        else:
+            response = False
     return response
