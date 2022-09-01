a7a702f09143eb0bcbc1e96f01d4c801c7d67da4
youstra89 <contact.youstra@gmail.com>
2022-09-01 21:51:24 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg_number = len(sys.argv) - 1
+    if arg_number == 0:
+        print("{} arguments.".format(arg_number))
+    elif arg_number == 1:
+        print("{} argument:".format(arg_number))
+        print("{}: {}".format(1, sys.argv[1]))
+    else:
+        print("{} argument:".format(arg_number))
+        for i in range(1, arg_number + 1):
+            print("{}: {}".format(i, sys.argv[i]))
+        
