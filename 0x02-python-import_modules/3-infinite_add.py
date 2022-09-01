8e07862bb2db00304c521610df7cf77a792ae24e
youstra89 <contact.youstra@gmail.com>
2022-09-01 22:04:01 +0000

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg_number = len(sys.argv) - 1
+    if arg_number == 1:
+        print("{}".format(int(sys.argv[1])))
+    if arg_number > 1:
+        sum = 0
+        for i in range(1, arg_number + 1):
+            sum = sum + int(sys.argv[i])
+        print("{}".format(sum))
