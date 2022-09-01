bd9a53ee2cf73bc36b33ee24c56b7d09d66c8e6f
youstra89 <contact.youstra@gmail.com>
2022-09-01 22:09:26 +0000

    Review 3-inifinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,8 @@
 if __name__ == "__main__":
     import sys
     arg_number = len(sys.argv) - 1
+    if arg_number == 0:
+        print('0')
     if arg_number == 1:
         print("{}".format(int(sys.argv[1])))
     if arg_number > 1:
