5701a113693d5515e81452114de559ebb0ba92ee
youstra89 <contact.youstra@gmail.com>
2022-09-06 09:30:35 +0000

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    else:
+        item = 0
+        key = None
+        for i in a_dictionary:
+            if a_dictionary[i] > item:
+                item = a_dictionary[i]
+                key = i
+        return key
