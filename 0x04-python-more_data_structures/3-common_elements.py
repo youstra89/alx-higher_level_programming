7c4722733e07dda085d3a350ec5e2a314b867b8e
youstra89 <contact.youstra@gmail.com>
2022-09-06 08:11:53 +0000

    The best solution for 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    my_set = set()
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                my_set.add(i)
-    return my_set
+    return set_1 & set_2
