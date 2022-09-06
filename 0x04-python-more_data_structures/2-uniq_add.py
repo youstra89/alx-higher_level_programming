98a749616b458a35564b823a5b9d99a3558ecb2b
youstra89 <contact.youstra@gmail.com>
2022-09-06 07:59:19 +0000

    Review 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-from functools import reduce
-
-
 def uniq_add(my_list=[]):
     lst = set(my_list)
-    # lst = list(filter(lambda x: x if x not in lst else None, my_list))
-    print(lst)
-    return reduce(lambda x, y: x + y, lst)
+    sum = 0
+    for i in lst:
+        sum += i
+    return sum
