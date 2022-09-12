208b81b8566eb70c860ca74095321f2a206b5f98
youstra89 <contact.youstra@gmail.com>
2022-09-12 17:28:44 +0000

    Review 2nd time task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError:
+    except Exception as ex:
         return False
     except:
         return False
