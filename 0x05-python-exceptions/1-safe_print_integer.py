6eb822ddc0ddf1912c50af502410a52780569206
youstra89 <contact.youstra@gmail.com>
2022-09-13 08:05:42 +0000

    Verify with isinstance

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
+        result = isinstance(value, int)
         print("{:d}".format(value))
-        return True
+        return result
     except Exception as ex:
         return False
     except:
