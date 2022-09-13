c59a404eb92f201d6a19c1a2c86fd0eb3bd31c95
youstra89 <contact.youstra@gmail.com>
2022-09-13 07:54:57 +0000

    Review last time 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,12 @@
 def safe_print_list(my_list=[], x=0):
     try:
         num = ''
-        last = None
+        j = 0
         for i in my_list[0:x]:
             num = num + str(i)
-            last = i - 1
+            j += 1
             # num += "%s" % i
         print(f"{num}")
-        return my_list[last]
-    except Exception as ex:
-        print(ex)
+        return j
+    except Exception:
+        print("ex")
